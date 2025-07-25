c94483ec9f4814c777f65326b8cfe82d23a5f2dd
georgeaddiso <162060920+georgeaddiso@users.noreply.github.com>
2025-07-25 12:44:13 +1200

    25/07/2025

--- level.gd
+++ level.gd
@@ -1,13 +1,12 @@
 extends Node2D
 
-func _physics_process(delta):
-	$Killzone.position.y - $player.position.y
-	$killzone.postion.x += $player.SPEED*delta
-	
+func physics_process(delta):
+	$Killzone.position.y = $Player.position.y
+	$killzone.position.x += $Player.SPEED*delta
 
 
 func _on_killzone_body_entered(body):
-	var things = $killzone.get_overlapping_bodies()
+	var things = $Killzone.get_overlapping_bodies()
 	for t in things:
-		if t.has_method("Exploded"):
-			t.Explode() # Replace with function body.
+		if t.has_method("Explode"):
+			t.Explode()
