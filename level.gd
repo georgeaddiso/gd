efa73bdba2921f6782d570ce8c74cc7d4c7eba67
georgeaddiso <162060920+georgeaddiso@users.noreply.github.com>
2025-07-29 12:36:25 +1200

    Tilemap and killzone update

--- level.gd
+++ level.gd
@@ -1,12 +1,1 @@
 extends Node2D
-
-func physics_process(delta):
-	$Killzone.position.y = $Player.position.y
-	$killzone.position.x += $Player.SPEED*delta
-
-
-func _on_killzone_body_entered(body):
-	var things = $Killzone.get_overlapping_bodies()
-	for t in things:
-		if t.has_method("Explode"):
-			t.Explode()
