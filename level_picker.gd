995e5d4128c8bcefa88307763c9545b86ebaa6db
georgeaddiso <162060920+georgeaddiso@users.noreply.github.com>
2025-08-27 14:04:47 +1200

    Level 2 button fix maybe

--- level_picker.gd
+++ level_picker.gd
@@ -2,3 +2,10 @@ extends Control
 
 func _on_level_1_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://level.tscn")
+	
+	
+
+
+
+func _on_level_2_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://level_2.tscn")
