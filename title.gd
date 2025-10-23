94e556b813100c14bd00336311b4d9055b9a1a69
georgeaddiso <162060920+georgeaddiso@users.noreply.github.com>
2025-10-24 09:18:30 +1300

    Final Update (probably)

--- title.gd
+++ title.gd
@@ -4,4 +4,4 @@ func _on_play_pressed():
 	get_tree().change_scene_to_file("res://level_picker.tscn")
 
 func _on_button_pressed() -> void:
-	OS.shell_open("https://georgeaddison.itch.io/meadow-dash-gd")
+	OS.shell_open("https://georgeaddison.itch.io/")
